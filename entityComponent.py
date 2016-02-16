14dce1afee83144133dfc8c3b9a38aa12b790fa8
Matt Peeples <matt.peeples90@gmail.com>
2016-02-16 00:15:09 -0500

    Overhauled map drawing system, got rid of camera. Using CSV and Tile engine now.

--- entityComponent.py
+++ entityComponent.py
@@ -1,48 +1,90 @@
-import pygame
+import pygame, os
 
 #Components
 class Position:
-	def __init__(self, x, y):
-		self.x = x
-		self.y = y
-	def update(self, dx, dy):
-		self.x += dx
-		self.y += dy
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+    def update(self, dx, dy):
+        self.x += dx
+        self.y += dy
 
 class Movement:
-	def __init__(self, dx, dy, movespeed):
-		self.dy = dy
-		self.dx = dx
-		self.movespeed = movespeed
+    def __init__(self, movespeed):
+        self.up = False
+        self.down = False
+        self.left = False						
+        self.right = False
+        self.speed = movespeed
 
 class Graphic(pygame.sprite.Sprite):
-	def __init__(self, width, height, image):
-		pygame.sprite.Sprite.__init__(self)
-		self.image = pygame.Surface([width, height])
-		self.image.blit(image, (0,0))
+    def __init__(self, width, height, image):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = pygame.Surface([width, height])
+        self.image.blit(image, (0,0))
 
 class Map:
-	def __init__(self, background, position):
-		self.background = pygame.image.load(background)
-		self.position = (0,0)
+    def __init__(self, tileset, layout):
+        # tiles = dict in format {'name' : Tile Object}, all tiles used on this map
+        # layout = 2D list, each value is a tile name as listed in self.tiles
+        self.tileset = tileset
+        self.layout = layout
+        self.tiles = [[self.tileset.mappings[col] for col in row] for row in self.layout]
+        
+    def drawMap(self, displaySurf):
+        # appending each tile to the 2d array and blitting them
+        # displaySurf = pygame Surface to blit tiles onto
+        for col_idx, col in enumerate(self.tiles, start=0):
+            for row_idx, tile in enumerate(col, start=0):
+                # all tiles are 16x16 so x-y coords are index*16
+                coords = (row_idx*16, col_idx*16)
+                tile.drawTile(displaySurf, coords)
+                
+class Tileset:
+    def __init__(self, **kwargs):
+        self.mappings = {}
+        for k, v in kwargs.iteritems():
+            self.mappings.update({k: Tile(v['img'], v['collide'])})
 
+class Tile:
+    def __init__(self, img, collision):
+        # img = file path to tile graphic (16x16 png)        
+        # collision = boolean, true = impassible
+        fullpath = os.path.abspath(os.path.join('./', "{img}.png".format(img=img)))
+        self.img = pygame.image.load(fullpath)
+        self.collision = collision
+    
+    def drawTile(self, displaySurf, (x, y)):
+        # displaySurf = the surface we are blitting tile onto
+        displaySurf.blit(self.img, (x, y))
+        
 #Entities
 class Character:
-	def __init__(self, frames, position, movement):
-		self.frames = frames
-		self.position = position
-		self.movement = movement
-		self.moving = False
+    def __init__(self, frames, position, movement):
+        self.frames = frames
+        self.position = position
+        self.movement = movement
+    
+    def drawCharacter(self, displaySurf):
+        if self.movement.up or self.movement.down or self.movement.left or self.movement.right:
+            displaySurf.blit(self.frames.next(), (self.position.x, self.position.y))
+        else:
+            displaySurf.blit(self.frames.images[0], (self.position.x, self.position.y))
 
+
+'''
 class Camera:
-	def __init__(self, center, (width, height)):
-		self.center = center
-		self.size = (width, height)
-	def drawWithinBounds(self, surface, map, pc, npcs = []):
-		surface.blit(map.background, map.position)
-		if pc.moving: #Only animate the character if the character is actually moving. Otherwise, stay in standing frame
-			surface.blit(pc.frames.next(), (pc.position.x, pc.position.y))
-		else:
-			surface.blit(pc.frames.images[0], (pc.position.x, pc.position.y))
-		for npc in npcs:
-			surface.blit(npc.frames.next(), (npc.position.x, npc.position.y))
+# Each map should have a camera object. Starts centered on main character
+    # but should not extend beyond map bounds
+    def __init__(self, center, (width, height)):
+        self.center = center
+        self.size = (width, height)
+    def drawWithinBounds(self, surface, map, pc, npcs = []):
+        surface.blit(map.background, map.position)
+        if pc.movement.up or pc.movement.down or pc.movement.left or pc.movement.right: #Only animate the character if the character is actually moving. Otherwise, stay in standing frame
+            surface.blit(pc.frames.next(), (pc.position.x, pc.position.y))
+        else:
+            surface.blit(pc.frames.images[0], (pc.position.x, pc.position.y))
+        for npc in npcs:
+            surface.blit(npc.frames.next(), (npc.position.x, npc.position.y))
+'''
--- main.py
+++ main.py
@@ -1,111 +1,90 @@
-import pygame, random, entityComponent, gameUtils
+import pygame, csv, random, entityComponent, gameUtils
 
 #Window Size constant
-size = (640, 480)
+size = (640, 640)
 
 
 def main():
-	pygame.init()
-	screen = pygame.display.set_mode(size)
-	pygame.display.set_caption("Movey Abouty time")
-	clock = pygame.time.Clock()
-	done = False
+    pygame.init()
+    screen = pygame.display.set_mode(size)
+    pygame.display.set_caption("Walk about time")
+    clock = pygame.time.Clock()
+    done = False
 
-	char_sprites = gameUtils.SpriteSheet("./sprites.png")
+    char_sprites = gameUtils.SpriteSheet("./sprites.png")
 
-	blueHairedBack = char_sprites.load_strip((0,128,24,32), 3, -1)
-	blueHairedRight = char_sprites.load_strip((0,160,24,32), 3, -1)
-	blueHairedForward = char_sprites.load_strip((0,192,24,32), 3, -1)
-	blueHairedLeft = char_sprites.load_strip((0,224,24,32), 3, -1)
+    blueHairedBack = char_sprites.load_strip((0,128,24,32), 3, -1)
+    blueHairedRight = char_sprites.load_strip((0,160,24,32), 3, -1)
+    blueHairedForward = char_sprites.load_strip((0,192,24,32), 3, -1)
+    blueHairedLeft = char_sprites.load_strip((0,224,24,32), 3, -1)
 
-	gogglesBack = char_sprites.load_strip((144,0,24,32), 3, -1)
-	gogglesRight = char_sprites.load_strip((144,32,24,32), 3, -1)
-	gogglesForward = char_sprites.load_strip((144,64,24,32), 3, -1)
-	gogglesLeft = char_sprites.load_strip((144,96,24,32), 3, -1)
-	
-	forestMap = entityComponent.Map("./CTForestBG.png", (0,0))
+    gogglesBack = char_sprites.load_strip((144,0,24,32), 3, -1)
+    gogglesRight = char_sprites.load_strip((144,32,24,32), 3, -1)
+    gogglesForward = char_sprites.load_strip((144,64,24,32), 3, -1)
+    gogglesLeft = char_sprites.load_strip((144,96,24,32), 3, -1)
+    
+    frames = 5
+    frameIter = gameUtils.FrameIter(gogglesBack, True, frames)
 
-	frames = 5
-	frameIter = gameUtils.FrameIter(gogglesBack, True, frames)
+    mainDude = entityComponent.Character(frameIter, entityComponent.Position(size[0]/2,size[1]/2), entityComponent.Movement(2))
 
-	mainDude = entityComponent.Character(frameIter, entityComponent.Position(size[0]/2,size[1]/2), entityComponent.Movement(0,0, 2))
+    # Making the tileset
+    tileset = entityComponent.Tileset(
+        floor={'img':'purple-octagon-tile', 'collide':False},
+        nwall={'img':'purple-wall-north', 'collide':True},
+        swall={'img':'purple-wall-south', 'collide':True},
+        ewall={'img':'purple-wall-east', 'collide':True},
+        wwall={'img':'purple-wall-west', 'collide':True},
+        newall={'img':'purple-wall-northeast-corner', 'collide':True},
+        nwwall={'img':'purple-wall-northwest-corner', 'collide':True},
+        sewall={'img':'purple-wall-southeast-corner', 'collide':True},
+        swwall={'img':'purple-wall-southwest-corner', 'collide':True}
+    )
 
-	theCamera = entityComponent.Camera(mainDude.position, size)
+    # First room layout
+    layout = []
+    with open('test-floorplan.csv', mode='rb') as floorplan:
+        reader = csv.reader(floorplan,delimiter=',')
+        for row in reader:
+            layout.append(row)
+    firstMap = entityComponent.Map(tileset, layout)
 
-	while(not done):	#game loop
+    # game loop
+    while(not done):
+        # For each event (keypress, mouse click, etc.):
+        keys = pygame.key.get_pressed()
+        keyPressed = False
+        for event in pygame.event.get(): # User did something
+            # Use a chain of if statements to run code to handle each event.
+            if event.type == pygame.QUIT:
+                print 'quittin' 
+                done = True
+            elif event.type == pygame.KEYDOWN: 
+                keyPressed = True
+                if(event.key == pygame.K_ESCAPE):
+                    print 'quittin'
+                    done = True
+        if(keys[pygame.K_UP]):
+            mainDude.position.y -= mainDude.movement.speed
+            mainDude.frames.images = gogglesBack                    
+        elif(keys[pygame.K_DOWN]):
+            mainDude.position.y += mainDude.movement.speed
+            mainDude.frames.images = gogglesForward                    
+        elif(keys[pygame.K_LEFT]):
+            mainDude.position.x -= mainDude.movement.speed
+            mainDude.frames.images = gogglesLeft                    
+        elif(keys[pygame.K_RIGHT]):
+            mainDude.position.x += mainDude.movement.speed
+            mainDude.frames.images = gogglesRight                    
 
+        #	Clear the screen
+        screen.fill((0,0,0))
 
+        #	Draw everything
+        firstMap.drawMap(screen)
+        mainDude.drawCharacter(screen)
+        pygame.display.update()
 
-		#	For each event (keypress, mouse click, etc.):
-		for event in pygame.event.get(): # User did something
-			keys = pygame.key.get_pressed()
-			#	Use a chain of if statements to run code to handle each event.
-			if event.type == pygame.QUIT: # If user clicked close
-				done = True # Flag that we are done so we exit this loop
-			elif event.type == pygame.KEYDOWN: # 
-				if(keys[pygame.K_ESCAPE]):
-					print "We're outta here!"
-					done = True
-				if(keys[pygame.K_UP]):
-					mainDude.movement.dy =-1*(mainDude.movement.movespeed)
-					mainDude.moving = True
-				elif(keys[pygame.K_DOWN]):
-					mainDude.movement.dy = mainDude.movement.movespeed
-					mainDude.moving = True
-				elif(keys[pygame.K_LEFT]):
-					mainDude.movement.dx = -1*(mainDude.movement.movespeed)
-					mainDude.moving = True
-				elif(keys[pygame.K_RIGHT]):
-					mainDude.movement.dx = mainDude.movement.movespeed
-					mainDude.moving = True
-				#if(keys[pygame.K_RIGHT] and keys[pygame.K_LEFT]):
-				#	mainDude.movement.dx = 0
-				#if(keys[pygame.K_UP] and keys[pygame.K_DOWN]):
-				#	mainDude.movement.dy = 0
-			elif event.type == pygame.KEYUP:
-				# Up or down is still pressed, stop left and right movement
-				if (keys[pygame.K_DOWN] or keys[pygame.K_UP]):
-					mainDude.movement.dx = 0
-				# vica versa
-				elif (keys[pygame.K_LEFT] or keys[pygame.K_RIGHT]):
-					mainDude.movement.dy = 0
-				# no keys are pressed, stop it all
-				else:
-					mainDude.movement.dx = 0
-					mainDude.movement.dy = 0
-					mainDude.moving = False
-#			elif event.type == pygame.MOUSEMOTION:
-#				location = pygame.mouse.get_pos()
-#				print location
-#			elif event.type == pygame.MOUSEBUTTONDOWN:
-#				print "whoa buddy, lay off"
-#			elif event.type == pygame.MOUSEBUTTONUP:
-#				print "thanks, geez. Stop it then."
-
-		#	Run calculations to determine where objects move, what happens when objects collide, etc.
-		mainDude.position.update(mainDude.movement.dx, mainDude.movement.dy)
-
-
-		if(mainDude.movement.dx > 0): #Determine facing based on character's last direction.
-			mainDude.frames.images = gogglesRight
-		elif(mainDude.movement.dx < 0):
-			mainDude.frames.images = gogglesLeft
-		if(mainDude.movement.dy > 0):
-			mainDude.frames.images = gogglesForward
-		elif(mainDude.movement.dy < 0):
-			mainDude.frames.images = gogglesBack
-
-		theCamera.position = (mainDude.position.x, mainDude.position.y)
-
-		#	Clear the screen
-		screen.fill((0,0,0))
-
-		#	Draw everything
-		theCamera.drawWithinBounds(screen, forestMap, mainDude)
-		print theCamera.position
-
-		pygame.display.update()
-
-		msElapsed = clock.tick(30) #advances frame, 30 FPS
+        msElapsed = clock.tick(30) #advances frame, 30 FPS
 
 if __name__ == '__main__' : main()
